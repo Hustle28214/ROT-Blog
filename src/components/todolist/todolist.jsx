cbe62d799fe215584162e6ca3dc1821a6abef247
wavvXD! <2300864263@qq.com>
2024-08-12 00:42:10 +0800

    update /自动控制原理

--- src/components/todolist/todolist.jsx
+++ src/components/todolist/todolist.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import styles from "./todolist.module.css";
 
 export default function TodoList({ children }) {
-    const leftContent = React.Children.toArray(children).filter(child => child.type !== TodoListRight);
+    const fullContent = React.Children.toArray(children).filter(child => child.type === TodoListFull);
+    const leftContent = React.Children.toArray(children).filter(child => child.type !== TodoListRight && child.type !== TodoListFull);
     const rightContent = React.Children.toArray(children).find(child => child.type === TodoListRight);
 
     return (
         <div className={styles.todolistMain}>
+            {fullContent.length > 0 && (
+                <div className={styles.todolistFullContainer}>
+                    {fullContent}
+                </div>
+            )}
             <div className={styles.todolistLeftContainer}>
                 <div className={styles.todolistItem}>
                     {leftContent}
@@ -20,6 +26,14 @@ export default function TodoList({ children }) {
 }
 
 export const TodoListRight = ({children}) => {
+    return (
+        <div className={styles.todolistItem}>
+            {children}
+        </div>
+    );
+}
+
+export const TodoListFull = ({children}) => {
     return (
         <div className={styles.todolistItem}>
             {children}

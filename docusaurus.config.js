f149ee3f988cd2d435cec7c0c1790747ef07761e
wavvXD! <2300864263@qq.com>
2024-08-12 22:11:40 +0800

    update /docs /docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,18 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/Hustle28214/ROT-Blog/tree/master/',
+            feedOptions: {
+              type: 'all',
+              copyright: `Copyright © ${new Date().getFullYear()} Yanyuyedeng.`,
+              createFeedItems: async (params) => {
+                const {blogPosts, defaultCreateFeedItems, ...rest} = params;
+                return defaultCreateFeedItems({
+                  // keep only the 10 most recent blog posts in the feed
+                  blogPosts: blogPosts.filter((item, index) => index < 10),
+                  ...rest,
+                });
+              },
+            },
         },
         theme: {
           customCss: './src/css/custom.css',

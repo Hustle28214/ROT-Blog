f443dad2b1c7b9248e837e5819e68ceedfd419fa
wavvXD! <2300864263@qq.com>
2024-08-20 17:58:10 +0800

    update TOC

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -6,23 +6,20 @@ import styles from './styles.module.css';
 const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
 const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
 
-export default function TOC({className, wordCount, readingTime, ...props}) {
+export default function TOC({ className, wordCount, readingTime, ...props }) {
   return (
-    <>
-      <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
+    <div className={clsx(styles.tableOfContentsWrapper, 'thin-scrollbar', className)}>
+      <div className={styles.tableOfContents}>
         <TOCItems
           {...props}
           linkClassName={LINK_CLASS_NAME}
           linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
         />
+        <div className={styles.tocFooter}>
+          <p>本文字数：{wordCount} 字</p>
+          <p>预计阅读时间：{readingTime} 分钟</p>
+        </div>
       </div>
-      <div className={styles.tocContent}>
-        {props.children}
-      </div>
-      <div className={styles.tocFooter}>
-        <p>本文字数：{wordCount} 字</p>
-        <p>预计阅读时间：{readingTime} 分钟</p>
-      </div>
-    </>
+    </div>
   );
 }

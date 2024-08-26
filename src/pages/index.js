bcd5fc216cab5004b736a82a8ad11f1063b60e22
wavvXD! <2300864263@qq.com>
2024-08-26 16:05:27 +0800

    update i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,20 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          <Translate>
+            Leyan Cai
+          </Translate>
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">
+          <Translate>
+          Peach blossoms in the spring breeze, a cup of wine; Ten years' lamp in the night rain of the rivers and lakes.
+          </Translate>
+          </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/intro">
-            HELLO THATS ME 👉
+            <Translate>HELLO THATS ME 👉</Translate>
           </Link>
         </div>
       </div>

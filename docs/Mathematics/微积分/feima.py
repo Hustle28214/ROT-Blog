27ca0c2c1e94f8522656c8df807254aab169e1bb
wavvXD! <2300864263@qq.com>
2024-11-16 19:31:53 +0800

    update

--- docs/Mathematics/微积分/feima.py
+++ docs/Mathematics/微积分/feima.py
@@ -1,12 +1,34 @@
-def solution(S: str) -> int:
-    ans = 0
+import numpy as np
+import cv2 as cv
 
 
+def GetRed(img):
+    """
+    提取图中的红色部分
+    """
+    # 转化为hsv空间
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    print(hsv.shape)
 
-    return ans
+    # 颜色在HSV空间下的上下限156-180还能改成0-10
+    low_hsv = np.array([156, 43, 46])
+    high_hsv = np.array([180, 255, 255])
+
+    # 使用opencv的inRange函数提取颜色
+    mask = cv2.inRange(hsv, lowerb=low_hsv, upperb=high_hsv)
+    Red = cv2.bitwise_and(img, img, mask=mask)
+
+    return Red
+ 
+
+
 
 if __name__ == '__main__':
-    import sys
-    str = sys.stdin.read()
-    str2 = str.split('"',2)
-    print(str[1])
+    src = "2.png"
+    img = cv.imread(src)
+
+    # 红色提取出来
+    Red = GetRed(img)
+
+    cv.imshow("Red", Red)
+    cv.waitKey()

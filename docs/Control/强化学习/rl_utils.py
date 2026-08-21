a2064800e42d4f4386f2fcc4c5f70c0215eeaef0
caileyan <2300864263@qq.com>
2026-08-21 19:31:55 +0800

    update

--- docs/Control/强化学习/rl_utils.py
+++ docs/Control/强化学习/rl_utils.py
@@ -4,6 +4,7 @@ import torch
 import collections
 import random
 
+
 class ReplayBuffer:
     def __init__(self, capacity):
         self.buffer = collections.deque(maxlen=capacity) 
@@ -27,23 +28,28 @@ def moving_average(a, window_size):
     end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
     return np.concatenate((begin, middle, end))
 
-def train_on_policy_agent(env, agent, num_episodes):
+def train_on_policy_agent(env, agent, num_episodes, seed=None):
     return_list = []
+    if seed is not None:
+        env.action_space.seed(seed)
+    episode_index = 0
     for i in range(10):
         with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
             for i_episode in range(int(num_episodes/10)):
                 episode_return = 0
                 transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
-                state = env.reset()
-                done = False
-                while not done:
+                state, _ = env.reset(seed=seed if episode_index == 0 else None)
+                episode_done = False
+                while not episode_done:
                     action = agent.take_action(state)
-                    next_state, reward, done, _ = env.step(action)
+                    next_state, reward, terminated, truncated, _ = env.step(action)
+                    episode_done = terminated or truncated
                     transition_dict['states'].append(state)
                     transition_dict['actions'].append(action)
                     transition_dict['next_states'].append(next_state)
                     transition_dict['rewards'].append(reward)
-                    transition_dict['dones'].append(done)
+                    # 时间上限截断仍允许价值函数从下一状态 bootstrap。
+                    transition_dict['dones'].append(terminated)
                     state = next_state
                     episode_return += reward
                 return_list.append(episode_return)
@@ -51,20 +57,27 @@ def train_on_policy_agent(env, agent, num_episodes):
                 if (i_episode+1) % 10 == 0:
                     pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
                 pbar.update(1)
+                episode_index += 1
     return return_list
 
-def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size):
+
+def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size,
+                           batch_size, seed=None):
     return_list = []
+    if seed is not None:
+        env.action_space.seed(seed)
+    episode_index = 0
     for i in range(10):
         with tqdm(total=int(num_episodes/10), desc='Iteration %d' % i) as pbar:
             for i_episode in range(int(num_episodes/10)):
                 episode_return = 0
-                state = env.reset()
-                done = False
-                while not done:
+                state, _ = env.reset(seed=seed if episode_index == 0 else None)
+                episode_done = False
+                while not episode_done:
                     action = agent.take_action(state)
-                    next_state, reward, done, _ = env.step(action)
-                    replay_buffer.add(state, action, reward, next_state, done)
+                    next_state, reward, terminated, truncated, _ = env.step(action)
+                    episode_done = terminated or truncated
+                    replay_buffer.add(state, action, reward, next_state, terminated)
                     state = next_state
                     episode_return += reward
                     if replay_buffer.size() > minimal_size:
@@ -75,6 +88,7 @@ def train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size
                 if (i_episode+1) % 10 == 0:
                     pbar.set_postfix({'episode': '%d' % (num_episodes/10 * i + i_episode+1), 'return': '%.3f' % np.mean(return_list[-10:])})
                 pbar.update(1)
+                episode_index += 1
     return return_list
 
 
